00eccc95e9575305d3c29bbb1494d0b99c11c5be
Lasske <storm0071@ya.ru>
2021-10-03 15:35:05 +0300

    delete

--- db/tables/phone_type.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-
-from ..base import Base
-
-
-class PhoneType(Base):
-    __tablename__ = "phones_types"
-    id = Column(String(50), primary_key=True, nullable=False)
-    name = Column(String(50), nullable=False)
--- db/tables/user_type.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, String
-
-from ..base import Base
-
-
-class UserType(Base):
-    __tablename__ = "users_types"
-    id = Column(String(50), primary_key=True, nullable=False)
-    name = Column(String(50), nullable=False)
